ce5091b9598e0907cfddf9c286bf9696d530af22
SlayerSj22 <112235877+SlayerSj22@users.noreply.github.com>
2024-01-19 12:40:54 +0530

    all

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todo'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Let's Fuck Redux</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
